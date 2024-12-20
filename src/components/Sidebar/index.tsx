c17a041ba22690f6ced068c8e4fbe2599a6f7ca2
Mr.Pattanasak <111237717+PattanasakGit@users.noreply.github.com>
2024-12-21 00:24:10 +0700

    [UPFATE] update side bar add user logout layout

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -13,6 +13,11 @@ import {
   SidebarSeparator,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Logout03Icon } from "hugeicons-react";
+
+import appConfig from "@/constants/appConfig";
+import ButtonTheme from "../Button";
 
 interface MenuItemProps {
   title: string;
@@ -20,28 +25,51 @@ interface MenuItemProps {
   icon: React.FC;
 }
 
-const LogoContent = ({ sideBarOpen }: { sideBarOpen: boolean }) => {
-  return (
-    <div className="flex items-center justify-center p-4 w-full ">
-      {sideBarOpen ? (
-        <span className="text-lg font-bold">Logo</span>
-      ) : (
-        <span className="text-lg font-bold">L</span>
-      )}
-    </div>
-  );
-};
-
-const UserContent = () => {
-  return (
-    <div className="flex items-center justify-center p-4 w-full">
-      <span className="text-lg font-bold">User</span>
-    </div>
-  );
-};
-
 export function AppSidebar({ items }: { items: MenuItemProps[] }) {
   const { open } = useSidebar();
+  const userData = {
+    name: "Pattanasak Atakul",
+    email: "test@example.com",
+  };
+
+  const LogoContent = ({ sideBarOpen }: { sideBarOpen: boolean }) => {
+    return (
+      <div className="flex items-center justify-center p-4 w-full ">
+        {sideBarOpen ? (
+          <span className="text-lg font-bold">{appConfig.app_name}</span>
+        ) : (
+          <span className="text-lg font-bold">{appConfig.app_name[0]}</span>
+        )}
+      </div>
+    );
+  };
+
+  const UserContent = () => {
+    return open ? (
+      <Card className="w-full flex flex-col items-center justify-center">
+        <CardHeader>
+          <CardTitle>{userData.name}</CardTitle>
+        </CardHeader>
+        <CardFooter className="w-full">
+          <ButtonTheme
+            customStyle="danger"
+            className="w-full"
+            icon={<Logout03Icon className="h-4 w-4" />}
+          >
+            Logout
+          </ButtonTheme>
+        </CardFooter>
+      </Card>
+    ) : (
+      <ButtonTheme
+        customStyle="gradientBlue"
+        className="w-full"
+      >
+        {userData.name[0]}
+      </ButtonTheme>
+    );
+  };
+
   return (
     <Sidebar collapsible="icon">
       <SidebarContent>
